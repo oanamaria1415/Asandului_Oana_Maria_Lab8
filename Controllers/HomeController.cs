b1b7154361adf2e9d18abb84d9b5ddbfb850f1ee
Oana-Maria Asandului <oanamaria1415@yahoo.com>
2026-01-13 23:15:54 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Asandului_Oana_Maria_Lab8.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Asandului_Oana_Maria_Lab8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/MovieControlller.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using MovieRecommenderGrpcService;
+
+public class MovieController : Controller
+{
+    private readonly GrpcChannel channel;
+    public MovieController()
+    {
+        //se va modifica portul corespunzator
+        channel = GrpcChannel.ForAddress("https://localhost:7281");
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Recommend()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Recommend(float userId, float
+   movieId)
+    {
+        var client = new
+       MovieRecommender.MovieRecommenderClient(channel);
+        var request = new RecommendationRequest
+        {
+            UserId = userId,
+            MovieId = movieId
+        };
+        var reply = client.Recommend(request);
+        ViewBag.UserId = userId;
+        ViewBag.MovieId = movieId;
+        ViewBag.Score = reply.Score;
+        ViewBag.Recommended = reply.Recommended;
+        return View();
+    }
+}
